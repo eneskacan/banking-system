17b4364f6aac1ecebac3e2e605ab35245ccda196
eneskacan <enes.kacan@agu.edu.tr>
2022-07-12 21:56:33 +0300

    Init Spring Boot project

---
+++ src/test/java/com/eneskacan/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eneskacan.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
